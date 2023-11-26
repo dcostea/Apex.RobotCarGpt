c4ed6d84b2d672a46fb3b9c594b84ff48cbb48fa
Daniel Costea <daniel_costea@ymail.com>
2023-11-26 15:58:39 +0100

    Optimize for demo.

--- Commands/CommandExtensions.cs
+++ Commands/CommandExtensions.cs
@@ -1,7 +1,84 @@
-﻿namespace Commands;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.Planners;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.TemplateEngine;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI;
+
+namespace Commands;
 
 public static class CommandExtensions
 {
+    public const string PluginsFolder = "plugins";
+    public const string MotorPlugin = nameof(MotorPlugin);
+    public const string BasicCommands = "go forward, go backward, turn left, turn right, and stop";
+
+    public const string ExtractBasicCommands = nameof(ExtractBasicCommands);
+    public const string ExtractBasicCommandsPromptTemplate = """
+You are a robot car capable of performing only the following allowed basic commands: {{ $commands }}.
+Initial state of the car is stopped. The last state of the car is stopped.
+You need to:
+[START ACTION TO BE PERFORMED]
+{{ $input }}
+[END ACTION TO BE PERFORMED]
+Extract a list of basic commands from the action to be performed to fulfill the goal.
+Restrict the extracted list to the allowed basic commands enumerated above.
+Give me the list as a comma separated list. Remove any introduction, ending or explanation from the response, show me only the list of allowed commands.
+""";
+    public static readonly PromptTemplateConfig ExtractBasicCommandsPromptTemplateConfig = new()
+    {
+        Description = "Extract basic motor commands.",
+        ModelSettings =
+        [
+            new OpenAIRequestSettings
+            {
+                MaxTokens = 500,
+                Temperature = 0.0
+            }
+        ],
+        Input = new PromptTemplateConfig.InputConfig
+        {
+            Parameters =
+            [
+                new() { Name = "input", Description = "Action to be performed." },
+                new() { Name = "commands", Description = "The commands to chose from." },
+            ]
+        }
+    };
+
+    public const string ExtractMostRelevantBasicCommand = nameof(ExtractMostRelevantBasicCommand);
+    public const string ExtractMostRelevantBasicCommandPromptTemplate = """
+You are a robot car capable of performing only the following allowed basic commands: {{ $commands }}.
+Initial state of the car is stopped. The last state of the car is stopped.
+You need to:
+[START ACTION TO BE PERFORMED]
+{{ $input }}
+[END ACTION TO BE PERFORMED]
+Extract the most relevant basic command, one command only, for the action to be performed to fulfill the goal.
+Restrict the extracted basic command to one of the allowed basic commands enumerated above.
+Remove any introduction, ending or explanation from the response, show me only the extracted basic command.
+""";
+    public static readonly PromptTemplateConfig ExtractMostRelevantBasicCommandPromptTemplateConfig = new()
+    {
+        Description = "Extract most relevant basic motor command.",
+        ModelSettings =
+        [
+            new OpenAIRequestSettings
+            {
+                MaxTokens = 500,
+                Temperature = 0.0
+            }
+        ],
+        Input = new PromptTemplateConfig.InputConfig
+        {
+            Parameters =
+            [
+                new() { Name = "input", Description = "Action to be performed." },
+                new() { Name = "commands", Description = "The commands to chose from." },
+            ]
+        }
+    };
+
     public static string ToArrow(this string function)
     {
         Console.OutputEncoding = System.Text.Encoding.Unicode;
@@ -20,5 +97,96 @@ public static class CommandExtensions
         };
 
         return x;
+    }
+
+    public static async Task CreateAndExecuteSequentialPlan(this IKernel kernel, string ask, ContextVariables variables, ILogger logger)
+    {
+        // Create sequential plan
+
+        var config = new SequentialPlannerConfig();
+        config.ExcludedFunctions.Add(ExtractBasicCommands);             // we don't want to allow this semantic function in the plan!
+        config.ExcludedFunctions.Add(ExtractMostRelevantBasicCommand);  // we don't want to allow this semantic function in the plan!
+        var planner = new SequentialPlanner(kernel, config);
+
+        var plan = await planner.CreatePlanAsync(ask);
+
+        // show steps as arrows (function names converted to arrows)
+        var planStepsArrows = plan.Steps.Any()
+            ? string.Join(" ", plan.Steps.Select(s => s.Name.ToArrow()))
+            : "No steps!";
+
+        logger.LogInformation("  PLAN STEPS: {arrows}", planStepsArrows);
+
+        // Execute sequential plan
+        var result = await kernel.RunAsync(plan, variables);
+
+        logger.LogDebug("  SEQUENTIAL PLAN RESULT: {result}", result.FunctionResults.First().GetValue<string>());
+    }
+
+    /// <summary>
+    /// Predefined functions chain
+    /// </summary>
+    /// <param name="kernel"></param>
+    /// <param name="commands"></param>
+    /// <param name="variables"></param>
+    /// <param name="logger"></param>
+    /// <returns></returns>
+    public static async Task CreateAndExecuteFunctionsChain(this IKernel kernel, IEnumerable<string> commands, ContextVariables variables, ILogger logger)
+    {
+        var functions = new List<ISKFunction>();
+        foreach (var command in commands)
+        {
+            logger.LogInformation("  ADDING {function} TO FUNCTIONS CHAIN", command);
+            functions.Add(kernel.Functions.GetFunction(MotorPlugin, command));
+        }
+
+        // Execute functions chain
+        var result = await kernel.RunAsync(variables, functions.ToArray());
+
+        logger.LogDebug("  FUNCTIONS CHAIN RESULT: {result}", result.FunctionResults.First().GetValue<string>());
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="kernel"></param>
+    /// <param name="ask"></param>
+    /// <param name="variables"></param>
+    /// <param name="logger"></param>
+    /// <returns></returns>
+    public static async Task CreateAndExecuteFunctionsAsSequenceOfActionPlan(this IKernel kernel, string ask, ContextVariables variables, ILogger logger)
+    {
+        var functions = ask.Split(',');
+
+        logger.LogInformation("REFINED ASK SPLIT IN FUNCTIONS:");
+        foreach (var function in functions)
+        {
+            logger.LogInformation("  FUNCTION: {function}", function);
+            await kernel.CreateAndExecuteActionPlan(function!, variables, logger);
+        }
+    }
+
+    public static async Task CreateAndExecuteActionPlan(this IKernel kernel, string ask, ContextVariables variables, ILogger logger)
+    {
+        // Create action plan
+
+        var config = new ActionPlannerConfig();
+        config.ExcludedFunctions.Add(ExtractBasicCommands);             // we don't want to allow this semantic function in the plan!
+        config.ExcludedFunctions.Add(ExtractMostRelevantBasicCommand);  // we don't want to allow this semantic function in the plan!
+        var planner = new ActionPlanner(kernel, config);
+
+        var plan = await planner.CreatePlanAsync(ask);
+
+        // show steps as arrows (function names converted to arrows)
+        var planStepsArrows = plan.Steps.Any()
+            ? string.Join(" ", plan.Steps.Select(s => s.Name.ToArrow()))
+            : "No steps!";
+
+        logger.LogInformation("  PLAN STEPS: {arrows}", planStepsArrows);
+
+        // Execute action plan
+        var result = await kernel.RunAsync(plan, variables);
+
+        logger.LogDebug("  ACTION PLAN RESULT: {result}", result.FunctionResults.First().GetValue<string>());
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,21 +1,17 @@
 ﻿using Commands;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Connectors.AI.OpenAI;
 using Microsoft.SemanticKernel.Diagnostics;
 using Microsoft.SemanticKernel.Orchestration;
-using Microsoft.SemanticKernel.Planners;
-using Microsoft.SemanticKernel.Planning;
-using Microsoft.SemanticKernel.TemplateEngine.Basic;
-using Microsoft.SemanticKernel.TemplateEngine;
+using Plugins;
 using Serilog;
 
 var loggerFactory = new LoggerFactory()
     .AddSerilog(new LoggerConfiguration()
-        .MinimumLevel.Debug()
-        //.MinimumLevel.Information()
-        .MinimumLevel.Override("Microsoft.SemanticKernel", Serilog.Events.LogEventLevel.Debug)
-        //.MinimumLevel.Override("Microsoft.SemanticKernel", Serilog.Events.LogEventLevel.Warning)
+        //.MinimumLevel.Debug()
+        .MinimumLevel.Information()
+        //.MinimumLevel.Override("Microsoft.SemanticKernel", Serilog.Events.LogEventLevel.Debug)
+        .MinimumLevel.Override("Microsoft.SemanticKernel", Serilog.Events.LogEventLevel.Warning)
         .WriteTo.Console()
         .CreateLogger()
     );
@@ -25,151 +21,95 @@ var kernel = new KernelBuilder()
     .WithLoggerFactory(loggerFactory)
     .Build();
 
-const string Commands = "go forward, go backward, turn left, turn right, and stop";
-const string MotorPlugin = nameof(MotorPlugin);
-const string ExtractBasicMotorCommands = nameof(ExtractBasicMotorCommands);
+var logger = kernel.LoggerFactory.CreateLogger(CommandExtensions.MotorPlugin);
 
-const string ExtractBasicMotorPromptTemplate = """
-You are a robot car capable of performing only the following allowed basic commands: {{ $commands }}.
-Initial state of the car is stopped. The last state of the car is stopped.
-You need to:
-[START ACTION TO BE PERFORMED]
-{{ $input }}
-[END ACTION TO BE PERFORMED]
-Create a comma separated list of basic commands, as enumerated above, to fulfill the goal.
-Remove any introduction, ending or explanation from the response, show me only the list of allowed commands.
-""";
+var motorPlugin = new MotorPlugin(logger);
 
-var logger = kernel.LoggerFactory.CreateLogger(MotorPlugin);
+// 1. Load semantic functions and native functions into the kernel
+
+// create semantic function inline (instead of importing it from MotorPlugin)
+////var extractBasicMotorCommandsSemanticFunction = kernel.CreateSemanticFunction(CommandExtensions.ExtractBasicCommandsPromptTemplate, CommandExtensions.ExtractBasicCommandsPromptTemplateConfig, CommandExtensions.ExtractBasicCommands, CommandExtensions.MotorPlugin);
+
+// import semantic functions from MotorPlugin
+var semanticMotorPluginFunctions = kernel.ImportSemanticFunctionsFromDirectory(Path.Combine(Directory.GetCurrentDirectory(), CommandExtensions.PluginsFolder), CommandExtensions.MotorPlugin);
+var extractBasicMotorCommandsSemanticFunction = semanticMotorPluginFunctions[CommandExtensions.ExtractBasicCommands];
+var extractMostRelevantBasicMotorCommandSemanticFunction = semanticMotorPluginFunctions[CommandExtensions.ExtractMostRelevantBasicCommand];
+
+_ = kernel.ImportFunctions(new Plugins.MotorPlugin(logger), CommandExtensions.MotorPlugin);
+
 
 var asks = new List<string>
 {
   "Go like forward forward turn right backward stop.",
-  "Go 10 steps where each step is a randomly selected step like: move forward, backward, and turning left or right.",
-  "You have a tree in front of the car. Avoid it.",
-  "Move forward, turn left, forward and return in the same place where it started.",
-  "Do a full circle by turning left followed by a full circle by turning right.",
+  //"Go 10 steps where each step is a randomly selected step like: move forward, backward, and turning left or right.",
+  //"You have a tree in front of the car. Avoid it.",
+  //"Move forward, turn left, forward and return in the same place where it started.",
+  //"Do a full circle by turning left followed by a full circle by turning right.",
   "Run away.",
-  "Do an evasive maneuver.",
-  "Do a pretty complex evasive maneuver with a least 15 steps. Stop at every 5 steps.",
-  "Do the moonwalk dancing.",
-  "Move like a jellyfish.",
+  //"Do an evasive maneuver.",
+  //"Do a pretty complex evasive maneuver with a least 15 steps. Stop at every 5 steps.",
+  //"Do the moonwalk dancing.",
+  //"Move like a jellyfish.",
   "Dance like a ballerina.",
-  "Go on square path.",
-  "Go on a full complete circle.",
-  "Go on a semi-circle.",
-  "Do a full 360 degrees rotation.",
+  //"Go on square path.",
+  //"Go on a full complete circle.",
+  //"Go on a semi-circle.",
+  //"Do a full 360 degrees rotation.",
 };
 
-var promptRenderer = new BasicPromptTemplateFactory();
-var promptTemplateConfig = new PromptTemplateConfig
-{
-    Description = "Extract basic motor commands.",
-    ModelSettings =
-    [
-        new OpenAIRequestSettings
-        {
-            MaxTokens = 500,
-            Temperature = 0.0
-        }
-    ],
-    Input = new PromptTemplateConfig.InputConfig
-    {
-        Parameters =
-        [
-            new() { Name = "input", Description = "Action to be performed." },
-            new() { Name = "commands", Description = "The commands to chose from." },
-        ]
-    }
-};
-var renderedPromptTemplate = promptRenderer.Create(ExtractBasicMotorPromptTemplate, promptTemplateConfig);
+// Create prompt renderers
+////var promptRenderer = new BasicPromptTemplateFactory();
+////var extractBasicCommandsRenderedPromptTemplate = promptRenderer.Create(CommandExtensions.ExtractBasicCommandsPromptTemplate, CommandExtensions.ExtractBasicCommandsPromptTemplateConfig);
+////var extractMostRelevantBasicCommandRenderedPromptTemplate = promptRenderer.Create(CommandExtensions.ExtractMostRelevantBasicCommandPromptTemplate, CommandExtensions.ExtractMostRelevantBasicCommandPromptTemplateConfig);
 
 
-// 0. Configure how to run
+// 2. Initialize context variables
 
-const bool CreatePlanWithRefinedAsk = true;
-const bool CreateInlineSemanticFunction = true;
-
-
-// 1. Load semantic functions and native functions into the kernel
-
-ISKFunction extractBasicMotorCommandsSemanticFunction = null!;
-if (CreateInlineSemanticFunction)
-{
-    // create semantic function inline (instead of importing it from MotorPlugin)
-    extractBasicMotorCommandsSemanticFunction = kernel.CreateSemanticFunction(ExtractBasicMotorPromptTemplate, promptTemplateConfig, ExtractBasicMotorCommands, MotorPlugin);
-}
-else 
-{
-    // import semantic function from MotorPlugin
-    var semanticMotorPluginFunctions = kernel.ImportSemanticFunctionsFromDirectory(Path.Combine(Directory.GetCurrentDirectory(), "plugins"), MotorPlugin);
-    extractBasicMotorCommandsSemanticFunction = semanticMotorPluginFunctions[ExtractBasicMotorCommands];
-}
-
-// import native functions from MotorPlugin
-_ = kernel.ImportFunctions(new Plugins.MotorPlugin(logger), MotorPlugin);
-
-var config = new SequentialPlannerConfig();
-config.ExcludedFunctions.Add(ExtractBasicMotorCommands);    // we don't want to use this function in the plan
-var planner = new SequentialPlanner(kernel, config);
+var variables = new ContextVariables();
+variables.Set("commands", CommandExtensions.BasicCommands);
 
 foreach (var ask in asks)
 {
-    Plan plan = null!;
-
-
-    // 2. Initialize context variables
-
     logger.LogInformation("----------------------------------------------------------------------------------------------------");
     logger.LogInformation("ASK: {ask}", ask);
 
-    var variables = new ContextVariables(ask);
-    variables.Set("commands", Commands);
+    variables.Update(ask);
 
-    var renderedPrompt = await renderedPromptTemplate.RenderAsync(kernel.CreateNewContext(variables));
-    logger.LogDebug("RENDERED PROMPT: {renderedPrompt}", renderedPrompt);
-
-
-    // 3. Create plan
+    // Rendered prompts
+    ////var extractBasicCommandsRenderedPrompt = await extractBasicCommandsRenderedPromptTemplate.RenderAsync(kernel.CreateNewContext(variables));
+    ////logger.LogDebug("RENDERED BASIC COMMANDS PROMPT: {renderedPrompt}", extractBasicCommandsRenderedPrompt);
+    ////var extractMostRelevantBasicCommandRenderedPrompt = await extractMostRelevantBasicCommandRenderedPromptTemplate.RenderAsync(kernel.CreateNewContext(variables));
+    ////logger.LogDebug("RENDERED MOST RELEVANT BASIC COMMAND PROMPT: {renderedPrompt}", extractMostRelevantBasicCommandRenderedPrompt);
 
     try
     {
-        if (!CreatePlanWithRefinedAsk)
-        {
-            plan = await planner.CreatePlanAsync(ask);
-        }
-        else
-        {
-            // refine ask
-            var refinedAskResult = await kernel.RunAsync(extractBasicMotorCommandsSemanticFunction, variables);
-            var refinedAsk = refinedAskResult.FunctionResults.First().GetValue<string>();
-            logger.LogInformation("EXTRACTED COMMANDS: {extracted}", refinedAsk);
+        #region One or more actions using original ask. Choose desired execution.
+        ////await kernel.CreateAndExecuteActionPlan(ask, variables, logger);
+        ////await kernel.CreateAndExecuteSequentialPlan(ask, variables, logger);
+        #endregion
 
-            plan = await planner.CreatePlanAsync(refinedAsk!);
-        }
+        #region One action using refined ask (as most relevant basic command)
+        ////var refinedAskMostRelevantResult = await kernel.RunAsync(extractMostRelevantBasicMotorCommandSemanticFunction, variables);
+        ////var refinedAskMostRelevant = refinedAskMostRelevantResult.FunctionResults.First().GetValue<string>();
+        ////logger.LogInformation("REFINED ASK (most relevant): {ask}", refinedAskMostRelevant);
 
-        // show steps as arrows (function names converted to arrows)
-        var planStepsArrows = plan.Steps.Any()
-            ? string.Join(" ", plan.Steps.Select(s => s.Name.ToArrow()))
-            : "No steps!";
-        logger.LogInformation("PLAN STEPS: {planStepsArrows}", planStepsArrows);
+        ////await kernel.CreateAndExecuteActionPlan(refinedAskMostRelevant!, variables, logger);
+        #endregion
+
+        #region More actions using refined ask (as list of basic commands). Choose desired execution.
+        var refinedAskListResult = await kernel.RunAsync(extractBasicMotorCommandsSemanticFunction, variables);
+        var refinedAskList = refinedAskListResult.FunctionResults.First().GetValue<string>();
+        logger.LogInformation("REFINED ASK (list): {ask}", refinedAskList);
+
+        ////await kernel.CreateAndExecuteFunctionsChain(new List<string> { nameof(motorPlugin.Forward), nameof(motorPlugin.Backward) }, variables, logger);
+        ////await kernel.CreateAndExecuteFunctionsAsSequenceOfActionPlan(refinedAskList!, variables, logger);
+        await kernel.CreateAndExecuteSequentialPlan(refinedAskList!, variables, logger);
+        #endregion
+
     }
     catch (SKException ex)
     {
-        logger.LogError("PLAN CREATION FAILED with exception: {message}", ex.Message);
+        logger.LogError("FAILED with exception: {message}", ex.Message);
         continue;
-    }
-
-
-    // 4. Execute plan
-
-    try
-    {
-        var result = await kernel.RunAsync(plan, variables);
-        logger.LogInformation("PLAN RESULT: {result}", result.FunctionResults.First().GetValue<string>());
-    }
-    catch (SKException ex)
-    {
-        logger.LogError("PLAN EXECUTION FAILED: {message}", ex.Message);
     }
 }
